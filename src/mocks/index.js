fd6c579c318448219955f8bb76f1334df10ab397
Gabriel <gabrielcfurlan@hotmail.com>
2024-09-04 22:20:59 -0300

    feature/PB-34-backend-pagination-logic - Ajuste dos titulos

--- src/mocks/index.js
+++ src/mocks/index.js
@@ -4,8 +4,7 @@ export const recentPosts = [
       "https://res.cloudinary.com/kentcdodds-com/image/upload/w_1100,q_auto,f_auto,b_rgb:e6e9ee/unsplash/photo-1639262498805-17c7dc422d37",
     date: "10-08-2023",
     category: "Travel",
-    title:
-      "0 A Journey Through Bohemiam Beauty: Exploring the Streets of Prague",
+    title: "A Journey Through Bohemiam Beauty: Exploring the Streets of Prague",
     link: "/",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
@@ -15,7 +14,7 @@ export const recentPosts = [
       "https://res.cloudinary.com/kentcdodds-com/image/upload/w_1100,q_auto,f_auto,b_rgb:e6e9ee/unsplash/photo-1639262498805-17c7dc422d37",
     date: "14-07-2023",
     category: "Fashion",
-    title: "1 Timeless Fashion with a Modern Twist Winter Collection",
+    title: "Timeless Fashion with a Modern Twist Winter Collection",
     link: "/",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
@@ -25,7 +24,7 @@ export const recentPosts = [
       "https://res.cloudinary.com/kentcdodds-com/image/upload/w_1100,q_auto,f_auto,b_rgb:e6e9ee/unsplash/photo-1639262498805-17c7dc422d37",
     date: "22-03-2023",
     category: "Food",
-    title: "2 My Favorite Authentic Italian Pasta Dishes",
+    title: "My Favorite Authentic Italian Pasta Dishes",
     link: "/",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
@@ -35,7 +34,7 @@ export const recentPosts = [
       "https://res.cloudinary.com/kentcdodds-com/image/upload/w_1100,q_auto,f_auto,b_rgb:e6e9ee/unsplash/photo-1639262498805-17c7dc422d37",
     date: "05-01-2023",
     category: "Food",
-    title: "3 Navigating First Impressions: Introduce Yourself",
+    title: "Navigating First Impressions: Introduce Yourself",
     link: "/",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
@@ -45,8 +44,7 @@ export const recentPosts = [
       "https://res.cloudinary.com/kentcdodds-com/image/upload/w_1100,q_auto,f_auto,b_rgb:e6e9ee/unsplash/photo-1639262498805-17c7dc422d37",
     date: "10-08-2023",
     category: "Travel",
-    title:
-      "4 A Journey Through Bohemiam Beauty: Exploring the Streets of Prague",
+    title: "A Journey Through Bohemiam Beauty: Exploring the Streets of Prague",
     link: "/",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
@@ -56,7 +54,7 @@ export const recentPosts = [
       "https://res.cloudinary.com/kentcdodds-com/image/upload/w_1100,q_auto,f_auto,b_rgb:e6e9ee/unsplash/photo-1639262498805-17c7dc422d37",
     date: "14-07-2023",
     category: "Fashion",
-    title: "5 Timeless Fashion with a Modern Twist Winter Collection",
+    title: "Timeless Fashion with a Modern Twist Winter Collection",
     link: "/",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
@@ -66,7 +64,7 @@ export const recentPosts = [
       "https://res.cloudinary.com/kentcdodds-com/image/upload/w_1100,q_auto,f_auto,b_rgb:e6e9ee/unsplash/photo-1639262498805-17c7dc422d37",
     date: "22-03-2023",
     category: "Food",
-    title: "6 My Favorite Authentic Italian Pasta Dishes",
+    title: "My Favorite Authentic Italian Pasta Dishes",
     link: "/",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
@@ -76,7 +74,7 @@ export const recentPosts = [
       "https://res.cloudinary.com/kentcdodds-com/image/upload/w_1100,q_auto,f_auto,b_rgb:e6e9ee/unsplash/photo-1639262498805-17c7dc422d37",
     date: "05-01-2023",
     category: "Food",
-    title: "7 Navigating First Impressions: Introduce Yourself",
+    title: "Navigating First Impressions: Introduce Yourself",
     link: "/",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
@@ -86,7 +84,7 @@ export const recentPosts = [
       "https://res.cloudinary.com/kentcdodds-com/image/upload/w_1100,q_auto,f_auto,b_rgb:e6e9ee/unsplash/photo-1639262498805-17c7dc422d37",
     date: "05-01-2023",
     category: "Food",
-    title: "8 Navigating First Impressions: Introduce Yourself",
+    title: "Navigating First Impressions: Introduce Yourself",
     link: "/",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
